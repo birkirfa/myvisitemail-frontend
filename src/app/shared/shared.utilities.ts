96d47f1102a64623419fabe686a45084796dfd75
dawedz <dariusz.wedzina@altimi.com>
2017-11-09 09:03:37 +0100

    changes to upload files

--- src/app/shared/shared.utilities.ts
+++ src/app/shared/shared.utilities.ts
@@ -1,4 +1,6 @@
 import { User } from './models/user.models';
+import { FileObject, FileType } from './models/common.models';
+
 
 export function isUser(obj: any): obj is User {
     if (obj && (obj instanceof User || obj.eMail !== undefined)) {
@@ -6,4 +8,30 @@ export function isUser(obj: any): obj is User {
     }
 
     return false;
+}
+
+export function convertToFileObject(input: HTMLInputElement, type: FileType, mimeType?: string): Promise<FileObject> {
+    return new Promise<FileObject>((resolve, reject) => {
+        const reader: FileReader = new FileReader();
+
+        if (!input.files || input.files.length === 0) {
+            return reject('No files selected!');
+        }
+        const file = input.files[0];
+
+        reader.onerror = (error) => {
+            return reject(error);
+        };
+        reader.onload = (readerResult) => {
+            const parsedFile = readerResult.target['result'];
+            const toUpload = {
+                file: parsedFile,
+                contentType: mimeType || file.type,
+                fileType: type
+            };
+            return resolve(toUpload);
+        };
+        reader.readAsDataURL(file);
+    });
+
 }
